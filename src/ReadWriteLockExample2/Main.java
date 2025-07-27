1885c2a1891c9d68081fd81bcf8a07629736f86f
priyanshi1399 <priyanshitomar001@gmail.com>
2025-07-28 03:15:33 +0530

    ReadWriteExample

--- src/ReadWriteLockExample2/Main.java
+++ src/ReadWriteLockExample2/Main.java
@@ -1,4 +1,28 @@
 package ReadWriteLockExample2;
 
 public class Main {
+    public static void main(String[] args) {
+    SharedResource resource=new SharedResource();
+
+    Thread t1=new Thread(()->{
+        resource.readResource();
+    });
+
+        Thread t2=new Thread(()->{
+            resource.readResource();
+        });
+
+        Thread t3=new Thread(()->{
+            resource.writeResources();
+        });
+
+        Thread t4=new Thread(()->{
+            resource.writeResources();
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
 }
--- src/ReadWriteLockExample2/SharedResource.java
+++ src/ReadWriteLockExample2/SharedResource.java
@@ -1,4 +1,46 @@
 package ReadWriteLockExample2;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class SharedResource {
+
+    ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    ReentrantReadWriteLock.ReadLock readLock=lock.readLock();
+    ReentrantReadWriteLock.WriteLock writeLock=lock.writeLock();
+
+    public void readResource(){
+        readLock.lock();
+        ;
+        try{
+            System.out.println(Thread.currentThread().getName() +" is reading");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            readLock.unlock();
+            System.out.println(Thread.currentThread().getName() +" finished reading");
+        }
+
+    }
+    public void writeResources(){
+        writeLock.lock();
+        ;
+        try{
+            System.out.println(Thread.currentThread().getName() +" is writing");
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getName() +" finished writing");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            writeLock.unlock();
+
+        }
+
+    }
+
+
+
 }
